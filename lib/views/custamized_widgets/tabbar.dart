03a6db14c07a2ee0992d439921d649f89fa5e37a
flutter <akashvs64731@gmail.com>
2023-12-27 15:55:07 +0530

    done

--- lib/views/custamized_widgets/tabbar.dart
+++ lib/views/custamized_widgets/tabbar.dart
@@ -1,114 +1,100 @@
-import 'package:flutter/material.dart';
-
-import '../../model/home_model.dart';
-
-class MyTabbedCard extends StatelessWidget {
-  late Loginmodel productModel;
-
-  @override
-Widget build(BuildContext context) {
-  return  Padding(
-    padding: EdgeInsets.only(bottom: 20,right: 35,top: 15,left: 35),
-    child: Card(color: Colors.white,
-      child: DefaultTabController(
-        length: 3, // Number of tabs
-        child: SizedBox(
-          child: Column(
-            children: [
-              TabBar(
-                labelColor: Colors.black,
-                tabs: [
-                  Tab(text: 'Feature'),
-                  Tab(text: 'Specifications'),
-                  Tab(text: 'Overview'),
-                ],
-              ),
-              Expanded(
-                child: TabBarView(
-                  children: [
-                    // Content for Tab 1
-                    SizedBox(
-                        width: double.infinity,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            SingleChildScrollView(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  ListView.builder(
-
-                                    itemCount: productModel.carname?.length
-                                    ,itemBuilder: (context, index) {
-                                    return Padding(padding: EdgeInsets.all(6),
-                                      child: Text(  "${productModel.carname}",),
-
-                                    );
-                                  },)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                                //   Padding(
-                                //     padding: EdgeInsets.all(6.0),
-                                //     child: Text('Central locking'),
-                                //   ),
-                                //   Padding(
-                                //     padding: EdgeInsets.all(6.0),
-                                //     child: Text('Driver air Bag'),
-                                //   ),
-                                //   Padding(
-                                //     padding: EdgeInsets.all(6.0),
-                                //     child: Text('Anti Lock Barking System'),
-                                //   ), Padding(
-                                //     padding: EdgeInsets.all(6.0),
-                                //     child: Text('Power door lock'),
-                                //   ), Padding(
-                                //     padding: EdgeInsets.all(6.0),
-                                //     child: Text('Adjustable seats'),
-                                //   ),
-                                //   Padding(
-                                //     padding: EdgeInsets.all(6.0),
-                                //     child: Text('Driver air Bag'),
-                                //   ),
-                                //   Padding(
-                                //     padding: EdgeInsets.all(6.0),
-                                //     child: Text('Crash sensor'),
-                                //   ), Padding(
-                                //     padding: EdgeInsets.all(6.0),
-                                //     child: Text('Air Conditioner'),
-                                //   ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        )),
-                    // Content for Tab 2
-                    Center(
-                      child: Text('Content for  2'),
-                    ),
-                    // Content for Tab 3
-                    Center(
-                      child: Text('Content for Tab 3'),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    ),
-  );
-}
-}
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:get/get_core/src/get_main.dart';
+//
+// import '../../controller/home_controller.dart';
+// import '../../model/home_model.dart';
+//
+// class MyTabbedCard extends StatelessWidget {
+//   late Loginmodel productModel;
+//
+//   MyTabbedCard({super.key, required this.productModel});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Padding(
+//       padding: EdgeInsets.only(bottom: 20, right: 35, top: 15, left: 35),
+//       child: Card(
+//         color: Colors.white,
+//         child: DefaultTabController(
+//           length: 3, // Number of tabs
+//           child: SizedBox(
+//             child: Column(
+//               children: [
+//                 TabBar(
+//                   labelColor: Colors.black,
+//                   tabs: [
+//                     Tab(text: 'Feature'),
+//                     Tab(text: 'Specifications'),
+//                     Tab(text: 'Overview'),
+//                   ],
+//                 ),
+//                 Expanded(
+//                   child: TabBarView(
+//                     children: [
+//                       // Content for Tab 1
+//                       SizedBox(
+//                           width: double.infinity,
+//                           child: Row(
+//                             mainAxisAlignment: MainAxisAlignment.start,
+//                             children: [
+//                               SingleChildScrollView(
+//                                 child: Column(
+//                                   crossAxisAlignment: CrossAxisAlignment.start,
+//                                   children: [
+//                                     Padding(
+//                                       padding: EdgeInsets.all(6.0),
+//                                       child: Text('Central locking'),
+//                                     ),
+//                                     Padding(
+//                                       padding: EdgeInsets.all(6.0),
+//                                       child: Text('Driver air Bag'),
+//                                     ),
+//                                     Padding(
+//                                       padding: EdgeInsets.all(6.0),
+//                                       child: Text('Anti Lock Barking System'),
+//                                     ),
+//                                     Padding(
+//                                       padding: EdgeInsets.all(6.0),
+//                                       child: Text('Power door lock'),
+//                                     ),
+//                                     Padding(
+//                                       padding: EdgeInsets.all(6.0),
+//                                       child: Text('Adjustable seats'),
+//                                     ),
+//                                     Padding(
+//                                       padding: EdgeInsets.all(6.0),
+//                                       child: Text('Driver air Bag'),
+//                                     ),
+//                                     Padding(
+//                                       padding: EdgeInsets.all(6.0),
+//                                       child: Text('Crash sensor'),
+//                                     ),
+//                                     Padding(
+//                                       padding: EdgeInsets.all(6.0),
+//                                       child: Text('Air Conditioner'),
+//                                     ),
+//                                   ],
+//                                 ),
+//                               ),
+//                             ],
+//                           )),
+//                       // Content for Tab 2
+//                       Center(
+//                         child: Text('Content for  2'),
+//                       ),
+//                       // Content for Tab 3
+//                       Center(
+//                         child: Text('Content for Tab 3'),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/views/widgets/cardetailspage.dart
+++ lib/views/widgets/cardetailspage.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:second_choice_flutter/model/home_model.dart';
-import '../../view/details_view.dart';
 import '../custamized_widgets/navigationbar_Details_page.dart';
 import '../custamized_widgets/tabbar.dart';
 
 class Detailspages extends StatefulWidget {
-   Loginmodel productModel;
+  late Loginmodel productModel;
 
   Detailspages({super.key, required this.productModel});
 
@@ -247,7 +246,7 @@ class _DetailspagesState extends State<Detailspages> {
                           ],
                         ),
                       ),
-                      Padding(
+                      const Padding(
                         padding: EdgeInsets.all(15.0),
                         child: Divider(),
                       )
@@ -285,13 +284,113 @@ class _DetailspagesState extends State<Detailspages> {
                 ),
               ),
               SizedBox(
-                height: 300,
-                width: double.infinity,
-                child: MyTabbedCard(),
-              )
+                  height: 300,
+                  width: double.infinity,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        bottom: 20, right: 35, top: 15, left: 35),
+                    child: Card(
+                      color: Colors.white,
+                      child: DefaultTabController(
+                        length: 3, // Number of tabs
+                        child: SizedBox(
+                          child: Column(
+                            children: [
+                              TabBar(
+                                labelColor: Colors.black,
+                                tabs: [
+                                  Tab(text: 'Feature'),
+                                  Tab(text: 'Specifications'),
+                                  Tab(text: 'Overview'),
+                                ],
+                              ),
+                              Expanded(
+                                child: TabBarView(
+                                  children: [
+                                    // Content for Tab 1
+                                    SizedBox(
+                                        width: double.infinity,
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          children: [
+                                            SingleChildScrollView(
+                                              child: Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  Padding(
+                                                    padding:
+                                                        EdgeInsets.all(6.0),
+                                                    child: Text(
+                                                      "${widget.productModel.carname}",                                                    ),
+                                                  ),
+                                                  const Padding(
+                                                    padding:
+                                                        EdgeInsets.all(6.0),
+                                                    child:
+                                                        Text('Driver air Bag'),
+                                                  ),
+                                                  const Padding(
+                                                    padding:
+                                                        EdgeInsets.all(6.0),
+                                                    child: Text(
+                                                        'Anti Lock Barking System'),
+                                                  ),
+                                                  const Padding(
+                                                    padding:
+                                                        EdgeInsets.all(6.0),
+                                                    child:
+                                                        Text('Power door lock'),
+                                                  ),
+                                                  Padding(
+                                                    padding:
+                                                        EdgeInsets.all(6.0),
+                                                    child: Text(
+                                                        'Adjustable seats'),
+                                                  ),
+                                                  Padding(
+                                                    padding:
+                                                        EdgeInsets.all(6.0),
+                                                    child:
+                                                        Text('Driver air Bag'),
+                                                  ),
+                                                  Padding(
+                                                    padding:
+                                                        EdgeInsets.all(6.0),
+                                                    child: Text('Crash sensor'),
+                                                  ),
+                                                  Padding(
+                                                    padding:
+                                                        EdgeInsets.all(6.0),
+                                                    child:
+                                                        Text('Air Conditioner'),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ],
+                                        )),
+                                    // Content for Tab 2
+                                    Center(
+                                      child: Text('Content for  2'),
+                                    ),
+                                    // Content for Tab 3
+                                    Center(
+                                      child: Text('Content for Tab 3'),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ))
             ],
           ),
         ),
-        bottomNavigationBar: NavigationBars());
+        bottomNavigationBar: const NavigationBars());
   }
 }
